8fcce4f467f073b1e570b64247850eeaf5b61b02
ha723-web <harshiniakunuri59@gmail.com>
2025-04-06 13:49:51 -0700

    Create sentiment_model.py

---
+++ app/sentiment_model.py
@@ -0,0 +1,13 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+def analyze_sentiment(text):
+    scores = analyzer.polarity_scores(text)
+    compound = scores['compound']
+    if compound >= 0.05:
+        return "Positive 😊"
+    elif compound <= -0.05:
+        return "Negative 😠"
+    else:
+        return "Neutral 😐"
